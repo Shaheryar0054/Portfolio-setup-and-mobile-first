07e1bf7605306c7c7ad7e3b740b1b71daf365246
Shaheryar Abid <sharyar0310@gmail.com>
2022-10-22 17:31:36 +0500

    Added javascript linters

--- validation.js
+++ validation.js
@@ -4,11 +4,11 @@ const errorMsg = document.querySelector('.errorMsg');
 
 // eslint-disable-next-line no-unused-vars
 function submitForm(event) {
+  event.preventDefault();
   if (emailElement.value === emailElement.value.toLowerCase()) {
     form.submit();
     form.reset();
   } else {
-    event.preventDefault();
     errorMsg.innerHTML = 'Email address should be in lowercase';
   }
 }
